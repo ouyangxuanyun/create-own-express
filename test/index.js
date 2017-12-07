90cd1e4b71fb1018fa4e156beb9538d9f49664f6
ouyangxuanyun <youtianzhan@126.com>
2017-12-07 23:41:32 +0800

    update

--- test/index.js
+++ test/index.js
@@ -2,3 +2,11 @@ const expross = require('../');
 const app = expross();
 // app.get();
 app.listen(3000);
+
+app.get('/', function (req, res) {
+    res.send('111 get Hello World!');
+});
+
+app.get('/', function (req, res) {
+    res.send('222 get Hello World!');
+});
